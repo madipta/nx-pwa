c1e064a5c794e24af1a4ddd40d867cb6474bb1c0
Madipta <madipta@gmail.com>
2021-01-26 09:37:18 +0700

    updates

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -5,9 +5,5 @@ export const appRoutes: Routes = [
     path: 'news',
     loadChildren: async() => (await import('./news/news.module')).NewsModule,
   },
-  {
-    path: 'jobs',
-    loadChildren: async() => (await import('./jobs/jobs.module')).JobsModule,
-  },
   { path: '**', redirectTo: 'news/newest', pathMatch: 'full' },
 ];
