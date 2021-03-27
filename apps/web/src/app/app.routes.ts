8f24a77dff028727b4a8e222e9e79933de7b50d9
Madipta <madipta@gmail.com>
2021-03-27 17:04:04 +0700

    update for vercel

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -2,21 +2,22 @@ import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
   {
-    path: 'news/newest',
-    loadChildren: async() => (await import('./news/news.module')).NewsModule,
+    path: '', 
+    pathMatch: 'full',
+    loadChildren: async () => (await import('./news/news.module')).NewsModule,
   },
   {
     path: 'news',
     children: [
       {
         path: 'item/:id',
-        loadChildren: async() => (await import('./news/news-detail/news-detail.module')).NewsDetailModule,
+        loadChildren: async () => (await import('./news/news-detail/news-detail.module')).NewsDetailModule,
       },
       {
         path: ':select',
-        loadChildren: async() => (await import('./news/news.module')).NewsModule,
+        loadChildren: async () => (await import('./news/news.module')).NewsModule,
       },
     ],
   },
-  { path: '**', redirectTo: '/news/newest', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
